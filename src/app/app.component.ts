8f226eb0af1e8294616ad7364c7b3bd9852827bf
William Fernandes <williamdu63800@gmail.com>
2021-01-25 18:59:10 +0100

    Add function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { start } from 'repl';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chronometre';
+
+  public isStarted = false;
+  public isPaused = false;
+
+  public start():void {
+    this.isStarted = true;
+  }
+
+  public pause():void {
+    this.isPaused = true;
+  }
 }
