9b0c6bc713ee0795b4bf03cb8dc2e2c8a24a139c
William Fernandes <williamdu63800@gmail.com>
2021-01-26 10:26:12 +0100

    Add a timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,39 @@ export class AppComponent {
 
   public isStarted = false;
   public isPaused = false;
+  public time = 0;
+  private idInterval : number|null = null;
 
   public start():void {
     this.isStarted = true;
+    this.isPaused = false;
+    this.startTimer();   
   }
 
   public pause():void {
     this.isPaused = true;
+    this.pauseTimer();
+  }
+
+  public stop():void{
+    this.stopTimer();
+    this.isStarted = false;
+  }
+
+  private startTimer():void {
+    this.idInterval = setInterval(() => {
+      this.time++;
+    }, 1);
+  }
+
+  private pauseTimer():void {
+    if(this.idInterval !== null){
+      clearInterval(this.idInterval);
+    }
+  }
+
+  private stopTimer(): void {
+    this.pauseTimer();
+    this.time = 0;
   }
 }
